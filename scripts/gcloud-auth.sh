dacc970aee9068f6791f272d37d959f248d6415a
Thomas Gaubert <tpgaubert@gmail.com>
2017-03-20 16:55:01 -0500

    Start of .travis.yml

---
+++ scripts/gcloud-auth.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# TODO: Actually authenticate
+
+# gcloud auth activate-service-account --key-file client-secret.json
+# gcloud config set project $GCLOUD_PROJECT
+# ssh-keygen -q -N "" -f ~/.ssh/google_compute_engine
---
+++ scripts/gcloud-deploy.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# TODO: Deploy to server
+
+# If on master and not a pull request, deploy to production
+# if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+#  gcloud compute ssh $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --command "./deploy-prod.sh"
+# fi
+
+# gcloud compute instances remove-metadata $GCLOUD_INSTANCE --zone $GCLOUD_ZONE --keys ssh-keys
