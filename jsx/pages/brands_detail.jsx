deb0eb391168303a166a3caef285b5f2b521b2d6
Drew Romanyk <drewiswaycool@gmail.com>
2017-03-22 15:09:00 -0500

    Get brand details from api

--- jsx/pages/brands_detail.jsx
+++ jsx/pages/brands_detail.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const Brands_Detail = ({ id }) => (
-	<div>
-        <h2>Specific Brand</h2>
-        <p>{id}</p>
-    </div>
-);
-export default Brands_Detail;
+export default class Brands_Details extends React.Component {
+    constructor(props) {
+        super(props);
+        this.id = props.id;
+        this.state = {brand: {name: '', avg_price: ''}}
+    }
+
+    componentDidMount() {
+        $.getJSON('http://0.0.0.0:8080/api/brands/' + this.id)
+            .then((data) => {
+                this.setState({brand: data});
+            });
+    }
+
+    render() {
+        const brand = (
+            <div>
+                <h1>{this.state.brand.name}</h1>
+                <p>{this.state.brand.avg_price}</p>
+            </div>
+        );
+        return (
+            <div>{ brand }</div>
+        );
+    }
+}
