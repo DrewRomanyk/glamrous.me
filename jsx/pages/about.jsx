80599e0ba30ff3057d0292205ed68bb06ddb0aa1
Drew Romanyk <drewiswaycool@gmail.com>
2017-03-23 10:24:13 -0500

    Add about page details

--- jsx/pages/about.jsx
+++ jsx/pages/about.jsx
@@ -1,6 +1,83 @@
 import React from 'react';
 
 const About = () => (
-	<div>About</div>
+    <div className="container">
+
+        <div className="row">
+            <div className="col-lg-12">
+                <h2 className="page-header">Meet The Glam Fam</h2>
+                <p>Welcome to glamrous.me! This web app is a way to get access to all of your favorite cosmetics in one
+                    quick glance.</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="/static/img/thomas.jpg" alt=""/>
+                <h3>Thomas Gaubert</h3>
+                <h6>Database & Backend</h6>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="/static/img/drew.jpg" alt=""/>
+                <h3>Drew Romanyk</h3>
+                <h6>Database & Backend</h6>
+                <p>I'm currently a junior computer science student at The University of Texas at Austin. I'm the
+                    president of MAD, and currently going to intern with RetailMeNot over the summer.</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt=""/>
+                <h3>Cameron Piel</h3>
+                <h6>Database & Backend</h6>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt=""/>
+                <h3>Robert Perce</h3>
+                <h6>Database & Backend</h6>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt=""/>
+                <h3>Melody Park</h3>
+                <h6>Database & Backend</h6>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div className="col-lg-4 col-sm-6 text-center">
+                <img className="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt=""/>
+                <h3>Thomas Potnuru</h3>
+                <h6>Database & Backend</h6>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-lg-4">
+                <h1 className="page-header">Stats</h1>
+                <ul>
+                    <li>Number of commits: ##</li>
+                    <li>Number of issues: ##</li>
+                    <li>Number of unit tests: ##</li>
+                    <li><a href="http://docs.glamrousme.apiary.io/#">Apiary API</a></li>
+                    <li><a href="https://github.com/DrewRomanyk/glamrous.me/issues">Github Issue Tracker</a></li>
+                    <li><a href="https://github.com/DrewRomanyk/glamrous.me">Github Repo</a></li>
+                </ul>
+            </div>
+            <div className="col-lg-4">
+                <h1 className="page-header">Data</h1>
+                <li><a href="http://makeup-api.herokuapp.com/">Makeup API</a> - used to collect all the information
+                    about each cosmetics brand, product, and details
+                    about those objects.
+                </li>
+            </div>
+            <div className="col-lg-4">
+                <h1 className="page-header">Tools</h1>
+                <li>Bootstrap - Frontend framework to simplify building pages.</li>
+                <li>React - Frontend engine to power all of our clientside rendering and routing.</li>
+                <li>Flask - Python server engine to power all the api and forward all non-api routing to React.</li>
+                <li>Slack - Group messaging to communicate effectively as a group.</li>
+                <li>Github - A git repository hosting service.</li>
+                <li>Apiary - An API documentation service.</li>
+            </div>
+        </div>
+    </div>
+
 );
 export default About;
