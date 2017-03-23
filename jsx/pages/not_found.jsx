2b6c020c03e40232c6741e1e636b247dd5313484
Thomas Gaubert <ThomasGaubert@users.noreply.github.com>
2017-03-23 18:11:19 -0500

    Add over-engineered 404 page (#17)
    * Add over-engineered 404 page
    
    * Tabs -> spaces

--- jsx/pages/not_found.jsx
+++ jsx/pages/not_found.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
 
+function getGif() {
+    var gifs = [
+        'confused.gif',
+        'donald.gif',
+        'wow.gif'
+    ];
+
+    var text = [
+        'Where are we?',
+        'Yeah, he\'s just as lost...',
+        'Wooooow'
+    ]
+
+    var randomIndex = Math.floor(Math.random() * gifs.length); 
+    return {gif: gifs[randomIndex], text: text[randomIndex]};
+}
+
+var g = getGif();
+var gif = g.gif;
+var text = g.text;
+var c = gif === "confused.gif" ? "bottom-fix" : "";
+
 const Not_Found = () => (
-	<div>Not Found - 404</div>
+    <div className="container center">
+        <h1 className="center">404</h1>
+        <p><b>Page not found - <a href="/">Go home?</a></b></p>
+
+        <img className={c} src={"/static/img/404/" + gif}/>
+
+        <p><small>{text}</small></p>
+    </div>
 );
 export default Not_Found;
