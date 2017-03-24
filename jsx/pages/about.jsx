906730e25084715f970a9478fc7b89e58ae9c706
Thomas Gaubert <tpgaubert@gmail.com>
2017-03-23 21:29:31 -0500

    git stats update

--- jsx/pages/about.jsx
+++ jsx/pages/about.jsx
@@ -19,9 +19,9 @@ const About = () => (
                 <h6>Devops & Frontend</h6>
                 <p>I'm an avid lover of the outdoors and the co-lead of Freetail Hackers. I'll be an intern at
                     Microsoft this summer.</p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 21</p>
+                <p>Number of issues: 3</p>
+                <p>Number of unit tests: 0</p>
             </div>
             <div className="col-lg-4 col-sm-6 text-center">
                 <img className="img-circle img-responsive img-center" src="/static/img/about/drew.jpg" alt=""/>
@@ -29,9 +29,9 @@ const About = () => (
                 <h6>Frontend & Backend</h6>
                 <p>I'm currently a junior computer science student at The University of Texas at Austin. I'm the
                     president of MAD, and currently going to intern with RetailMeNot over the summer.</p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 28</p>
+                <p>Number of issues: 4</p>
+                <p>Number of unit tests: 0</p>
             </div>
             <div className="col-lg-4 col-sm-6 text-center">
                 <img className="img-circle img-responsive img-center" src="/static/img/about/cameron.jpg" alt=""/>
@@ -39,9 +39,9 @@ const About = () => (
                 <h6>Database & Backend</h6>
                 <p>My name is Cameron Piel, I am a 5th year student here at UT. When I am not coding or cooking you can
                     find me sitting at my desk pretending to be productive.</p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 11</p>
+                <p>Number of issues: 3</p>
+                <p>Number of unit tests: 15</p>
             </div>
             <div className="col-lg-4 col-sm-6 text-center">
                 <img className="img-circle img-responsive img-center" src="static/img/about/rperce.jpg" alt="Robert Perce"/>
@@ -49,27 +49,27 @@ const About = () => (
                 <h6>Frontend & Devops</h6>
                 <p>Fourth-year Computer Science and Mathematics student at UT Austin. I'll be starting full-time at
                    Indeed in July!</p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 18</p>
+                <p>Number of issues: 3</p>
+                <p>Number of unit tests: 0</p>
             </div>
             <div className="col-lg-4 col-sm-6 text-center">
 				<img className="img-circle img-responsive img-center" src="static/img/about/mpark.jpg" alt="Melody Park, get it, hahahaha"/>
                 <h3>Melody Park</h3>
                 <h6>Frontend & Backend</h6>
                 <p>Fourth year UT student who likes to get swole. Weenie on the outside. Dragon on the inside.</p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 2</p>
+                <p>Number of issues: 1</p>
+                <p>Number of unit tests: 0</p>
             </div>
             <div className="col-lg-4 col-sm-6 text-center">
                 <img className="img-circle img-responsive img-center" src="/static/img/about/thomasp.jpg" alt=""/>
                 <h3>Thomas Potnuru</h3>
                 <h6>API & Backend</h6>
                 <p>Currently a senior at UT Austin doing my Bachelor's in Computer Sceince. I am a IEEE Computer Society Officer again this year! Usually making and eating dessert in my free time. </p>
-                <p>Number of commits: ##</p>
-                <p>Number of issues: ##</p>
-                <p>Number of unit tests: ##</p>
+                <p>Number of commits: 3</p>
+                <p>Number of issues: 1</p>
+                <p>Number of unit tests: 0</p>
             </div>
         </div>
 
@@ -77,9 +77,9 @@ const About = () => (
             <div className="col-lg-4">
                 <h1 className="page-header">Stats</h1>
                 <ul>
-                    <li>Number of commits: ##</li>
-                    <li>Number of issues: ##</li>
-                    <li>Number of unit tests: ##</li>
+                    <li>Number of commits: 97</li>
+                    <li>Number of issues: 15</li>
+                    <li>Number of unit tests: 15</li>
                     <li><a href="http://docs.glamrousme.apiary.io/#">Apiary API</a></li>
                     <li><a href="https://github.com/DrewRomanyk/glamrous.me/issues">Github Issue Tracker</a></li>
                     <li><a href="https://github.com/DrewRomanyk/glamrous.me">Github Repo</a></li>
