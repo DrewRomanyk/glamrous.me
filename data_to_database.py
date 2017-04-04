a507ffefce48e958705683bcbd2b6dc0a60690a0
Drew Romanyk <drewiswaycool@gmail.com>
2017-04-04 17:04:41 -0500

    Fix errors, and finish api to python data

--- data_to_database.py
+++ data_to_database.py
@@ -19,14 +19,14 @@ tags = {}
 
 
 def get_brand_image_from_bing(brand_name):
-    image_url_req = Request(
-        "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=smashbox&mkt=en-us&imageType=line&safeSearch=Strict")
-    image_url_req.add_header('Ocp-Apim-Subscription-Key', bing_api_key)
-    response = urlopen(image_url_req)
-    bing_image_response = json.load(response)
+    # image_url_req = Request(
+    #     "https://api.cognitive.microsoft.com/bing/v5.0/images/search?q=smashbox&mkt=en-us&imageType=line&safeSearch=Strict")
+    # image_url_req.add_header('Ocp-Apim-Subscription-Key', bing_api_key)
+    # response = urlopen(image_url_req)
+    # bing_image_response = json.load(response)
 
     # Find first image url
-    return bing_image_response['value'][0]['contentUrl']
+    return ''  # bing_image_response['value'][0]['contentUrl']
 
 
 def update_average(cur_avg, length, new_data):
@@ -40,10 +40,11 @@ def create_product(api_product):
         'id': api_product['id'],
         'name': api_product['name'],
         'description': api_product['description'],
-        'price': price,
-        'rating': rating,
+        'price': float(price),
+        'rating': float(rating),
         'image_url': api_product['image_link'],
-        'colors': api_product['product_colors']
+        'colors': api_product['product_colors'],
+        'tags': set()
     }
 
 
@@ -52,13 +53,14 @@ def create_or_update_brand(brand_name, product):
         # Create
         brands[brand_name] = {
             'id': len(brands),
-            'name': api_product['brand'],
+            'name': brand_name,
             'image_url': get_brand_image_from_bing(brand_name),
             'avg_price': product['price'],
             'avg_rating': product['rating'],
-            'products': [
-                product['id']
-            ],
+            'products': {
+                product['id'],
+            },
+            'categories': set(),
         }
     else:
         # Update
@@ -72,7 +74,7 @@ def create_or_update_brand(brand_name, product):
             len(brands[brand_name]['products']),
             product['rating']
         )
-        brands[brand_name]['products'].append(product['id'])
+        brands[brand_name]['products'] |= {product['id']}
 
 
 def create_or_update_category(category_name, is_root, product):
@@ -84,10 +86,11 @@ def create_or_update_category(category_name, is_root, product):
             'is_root': is_root,
             'avg_price': product['price'],
             'avg_rating': product['rating'],
-            'products': [
-                product['id']
-            ],
-            'sub_categories': [],
+            'products': {
+                product['id'],
+            },
+            'sub_categories': set(),
+            'brands': set()
         }
     else:
         # Update
@@ -101,7 +104,7 @@ def create_or_update_category(category_name, is_root, product):
             len(categories[category_name]['products']),
             product['rating']
         )
-        categories[category_name]['products'].append(product['id'])
+        categories[category_name]['products'] |= {product['id']}
 
 
 def create_or_update_tag(tag_name, product):
@@ -112,9 +115,10 @@ def create_or_update_tag(tag_name, product):
             'name': tag_name,
             'avg_price': product['price'],
             'avg_rating': product['rating'],
-            'products': [
-                product['id']
-            ],
+            'products': {
+                product['id'],
+            },
+            'brands': set()
         }
     else:
         # Update
@@ -128,32 +132,63 @@ def create_or_update_tag(tag_name, product):
             len(tags[tag_name]['products']),
             product['rating']
         )
-        tags[tag_name]['products'].append(product['id'])
+        tags[tag_name]['products'] |= {product['id']}
 
 
 get_brand_image_from_bing("smashbox")
 
-# for api_product in api_products:
-# product_id = api_product['id']
-# # Create Product
-# products[product_id] = create_product(api_product)
-#
-# # Create/Update Brand
-# brand_name = api_product['brand']
-# create_or_update_brand(brand_name, products[product_id])
-# products[product_id]['brand'] = brands[brand_name]['id']
-#
-# # Create/Update Category
-# category_name = api_product['product_type']
-# if category_name is not None:
-#     create_or_update_category(category_name, True, products[product_id])
-#
-# # Create/Update Sub-Category
-# sub_category_name = api_product['category']
-# if sub_category_name is not None:
-#     create_or_update_category(sub_category_name, False, products[product_id])
-#
-# # Create/Update Tags
-# tag_name_list = api_product['tag_list']
-# for tag_name in tag_name_list:
-#     create_or_update_tag(tag_name, products[product_id])
+for api_product in api_products:
+    product_id = api_product['id']
+    # Create Product
+    products[product_id] = create_product(api_product)
+
+    # Create/Update Brand
+    brand_name = api_product['brand']
+    create_or_update_brand(brand_name, products[product_id])
+    products[product_id]['brand'] = brands[brand_name]['id']
+
+    # Create/Update Category
+    category_name = api_product['product_type']
+    create_or_update_category(category_name, True, products[product_id])
+    products[product_id]['category'] = categories[category_name]['id']
+
+    # Create/Update Sub-Category
+    sub_category_name = api_product['category']
+    if sub_category_name is not None:
+        create_or_update_category(sub_category_name, False, products[product_id])
+        products[product_id]['sub_category'] = categories[sub_category_name]['id']
+        categories[category_name]['sub_categories'] |= {categories[sub_category_name]['id']}
+
+    # Create/Update Tags
+    tag_name_list = api_product['tag_list']
+    for tag_name in tag_name_list:
+        create_or_update_tag(tag_name, products[product_id])
+        products[product_id]['tags'] |= {tags[tag_name]['id']}
+        tags[tag_name]['brands'] |= {brands[brand_name]['id']}
+
+    # Relations
+    brands[brand_name]['categories'] |= {categories[category_name]['id']}
+    if sub_category_name is not None:
+        brands[brand_name]['categories'] |= {categories[sub_category_name]['id']}
+
+    categories[category_name]['brands'] |= {brands[brand_name]['id']}
+
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return list(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+with open('products.json', 'w') as f:
+    json.dump(products, f, cls=SetEncoder)
+
+with open('brands.json', 'w') as f:
+    json.dump(brands, f, cls=SetEncoder)
+
+with open('categories.json', 'w') as f:
+    json.dump(categories, f, cls=SetEncoder)
+
+with open('tags.json', 'w') as f:
+    json.dump(tags, f, cls=SetEncoder)
