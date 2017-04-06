c3e548471b08345dc7b19d5358dccaf7b088bcd0
Cameron Piel <cameron.piel@gitty.xyz>
2017-04-06 16:25:33 -0500

    More test stuff

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,4 @@
-from ..data_to_database
+from ... import data_to_database
 from unittest import main, TestCase
 from .models import Brand, Product, Color, Category, SubCategory, ProductCategory
 from .app import db
