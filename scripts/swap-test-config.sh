3f5f9ef687bc6ae2c4c97dd357c03646e6b39afb
Thomas Gaubert <tpgaubert@gmail.com>
2017-04-06 19:48:26 -0500

    fix again...

--- scripts/swap-test-config.sh
+++ scripts/swap-test-config.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-docker-ip() {
-  sudo docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$@"
-}
-
 [[ -e config.json ]] && cp config.json config.json.bak
 cp config.json.test config.json
-
-DB_IP=$(docker-ip glamrous-postgres)
-
-echo "IP address of postgres container is $DB_IP"
-
-echo "Creating configuration file..."
-sed -ie "s/IPADDRESS/${DB_IP}/g" config.json
