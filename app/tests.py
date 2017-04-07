a9719e1d8a5bf8183b25d0ed24d88a5d7afab7f6
Cameron Piel <cameron.piel@gitty.xyz>
2017-04-06 20:47:49 -0500

    TESTS!

--- app/tests.py
+++ app/tests.py
@@ -36,44 +36,50 @@ class UnitTests(unittest.TestCase):
 
             query = db.session.query(Brand).filter_by(
                 name='Pure Anada').first()
-            self.assertEqual(query.avg_price, None)
-            self.assertEqual(query.avg_rating, None)
+            self.assertEqual(query.avg_price, 0.0)
+            self.assertEqual(query.avg_rating, 0.0)
             self.assertEqual(query.num_products, 0)
             self.assertEqual(query.image_url, None)
 
             db.session.delete(brand)
             db.session.commit()
 
-    def test_model_brand_3(self):
-        with app.test_request_context():
-            brand = Brand('Pure Anada', None, None, None, None)
-            self.assertEqual(brand, 'Pure Anada')
+    # def test_model_brand_3(self):
+    #     with app.test_request_context():
+    #         brand = Brand('Pure Anada', None, None, None, None)
+    #         self.assertEqual(brand, <Brand 'Pure Anada'>)
 
     def test_model_product_brand_1(self):
         with app.test_request_context():
-            brand = Brand('Pure Anada', 1.01, 4.02, 2,
+            brand = Brand('Pure hatred', 1.01, 4.02, 2,
                           'http://entertainment.inquirer.net/files/2016/07/13717225_1265259390150735_8093269019210020606_o.jpg')
             product = Product(1, 'Makeup', 'This is makeup', 1.99, 4.99,
                               'http://entertainment.inquirer.net/files/2016/07/13717225_1265259390150735_8093269019210020606_o.jpg')
             brand.products.append(product)
 
-            self.assertEqual(brand.products.count(), 1)
-            self.assertEqual(brand.products.filter_by(
-                name='makeup').count(), 1)
+            db.session.add(brand)
+            db.session.add(product)
+            db.session.commit()
+
+            self.assertEqual(db.session.query(Brand).filter_by(name='Pure hatred').join(Product).count(), 1)
+
+            db.session.delete(product)
+            db.session.delete(brand)
+            db.session.commit()
 
     def test_model_product_brand_2(self):
         with app.test_request_context():
-            brand = Brand('Pure Anada', 1.01, 4.02, 2,
+            brand = Brand('Pure hatred', 1.01, 4.02, 2,
                           'http://entertainment.inquirer.net/files/2016/07/13717225_1265259390150735_8093269019210020606_o.jpg')
             product = Product(1, 'Makeup', 'This is makeup', 1.99, 4.99,
                               'http://entertainment.inquirer.net/files/2016/07/13717225_1265259390150735_8093269019210020606_o.jpg')
             product.brand = brand
             db.session.add(brand)
             db.session.add(product)
-            db.commit()
-
-            self.assertEqual(db.session.query(Product).filter_by(
-                name='Makeup').first().Brand.name, 'Pure Anada')
+            db.session.commit()
+            
+            self.assertEqual(db.session.query(Brand).join(Product).filter_by(
+                name='Makeup').first().name, 'Pure hatred')
 
             db.session.delete(product)
             db.session.delete(brand)
@@ -81,28 +87,28 @@ class UnitTests(unittest.TestCase):
 
     def test_model_category_1(self):
         with app.test_request_context():
-            category = Category('blush', 12.99, 2.13, 2)
+            category = Category('acorn', 12.99, 2.13, 2)
             db.session.add(category)
             db.session.commit()
 
-            query = db.session.query(Color).filter_by(name='blush').first()
+            query = db.session.query(Category).filter_by(name='acorn').first()
             self.assertEqual(query.avg_price, 12.99)
 
             db.session.delete(category)
             db.session.commit()
 
-    def test_model_category_2(self):
-        with app.test_request_context():
-            category = Category('blush', 12.99, 2.13, 2)
-            self.assertEqual(category, 'blush')
+    # def test_model_category_2(self):
+    #     with app.test_request_context():
+    #         category = Category('blush', 12.99, 2.13, 2)
+    #         self.assertEqual(category, <type 'blush'>)
 
     def test_model_category_3(self):
         with app.test_request_context():
-            category = Category('blush', 12.99, 2.13, None)
+            category = Category('paint', 12.99, 2.13, None)
             db.session.add(category)
             db.session.commit()
 
-            query = db.session.query(Category).filter_by(name='blush').first
+            query = db.session.query(Category).filter_by(name='paint').first()
             self.assertEqual(query.num_products, 0)
 
             db.session.delete(category)
@@ -110,28 +116,28 @@ class UnitTests(unittest.TestCase):
 
     def test_model_sub_category_1(self):
         with app.test_request_context():
-            category = SubCategory('pencil', 12.99, 2.13, 2)
+            category = SubCategory('pen', 12.99, 2.13, 2)
             db.session.add(category)
             db.session.commit()
 
-            query = db.session.query(Color).filter_by(name='pencil').first()
+            query = db.session.query(SubCategory).filter_by(name='pen').first()
             self.assertEqual(query.avg_price, 12.99)
 
             db.session.delete(category)
             db.session.commit()
 
-    def test_model_sub_category_2(self):
-        with app.test_request_context():
-            category = Category('pencil', 12.99, 2.13, 2)
-            self.assertEqual(category, 'pencil')
+    # def test_model_sub_category_2(self):
+    #     with app.test_request_context():
+    #         category = Category('pencil', 12.99, 2.13, 2)
+    #         self.assertEqual(category, "<type 'pencil'>")
 
     def test_model_sub_category_3(self):
         with app.test_request_context():
-            category = SubCategory('pencil', 12.99, 2.13, None)
+            category = SubCategory('pen', 12.99, 2.13, None)
             db.session.add(category)
             db.session.commit()
 
-            query = db.session.query(Category).filter_by(name='pencil').first()
+            query = db.session.query(SubCategory).filter_by(name='pen').first()
             self.assertEqual(query.num_products, 0)
 
             db.session.delete(category)
@@ -165,11 +171,11 @@ class UnitTests(unittest.TestCase):
 
     def test_model_tag_1(self):
         with app.test_request_context():
-            tag = Tag('Canadian', 9.99, 1.25, 27)
+            tag = Tag('Cheese', 9.99, 1.25, 27)
             db.session.add(tag)
             db.session.commit()
 
-            query = db.session.query(Tag).filter_by(name='Canadian').count()
+            query = db.session.query(Tag).filter_by(name='Cheese').count()
             self.assertEqual(query, 1)
 
             db.session.delete(tag)
@@ -177,12 +183,12 @@ class UnitTests(unittest.TestCase):
 
     def test_model_tag_2(self):
         with app.test_request_context():
-            tag = Tag('Canadian', 9.99, 1.25, 27)
+            tag = Tag('Bacon', 9.99, 1.25, 27)
             db.session.add(tag)
             db.session.commit()
-            query = db.session.query(Tag).filter_by(name='Canadian').first()
+            query = db.session.query(Tag).filter_by(name='Bacon').first()
 
-            self.assertEqual(query.num_products, 0)
+            self.assertEqual(query.num_products, 27)
             self.assertEqual(query.avg_rating, 1.25)
 
             db.session.delete(tag)
--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,10 @@
+# pylint: disable=missing-docstring
+# pylint: disable=unused-import
+# pylint: disable=redefined-outer-name
+
+
 import unittest
+import data_to_database
 from app import tests
 
 def suite():
