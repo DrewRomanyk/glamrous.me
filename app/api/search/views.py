7f4b1468ddcb4633dd29d26c19be5b5b956569da
Drew Romanyk <drewiswaycool@gmail.com>
2017-04-07 09:31:00 -0500

    Progress with searching

--- app/api/search/views.py
+++ app/api/search/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from app.models import Brand, Product, Category, Tag
 
 api_search_blueprints = Blueprint(
     'api_search', __name__
@@ -11,14 +12,29 @@ search_results = {
 }
 
 
+def search_for_keyword(keyword, content):
+    return content.find(keyword) != -1
+
+
 @api_search_blueprints.route('/api/search/<data>')
 def get_tag(data):
     keywords = data.split(' ')
     search_results['keywords'] = keywords
 
     # Search for keywords for the entire database
-    # If shown add to OR; if it has all keywords add to AND
+    results = dict()
+    for keyword in keywords:
+        results[keyword] = {}
+    for brand in Brand.query.all():
+        for keyword in keywords:
+            if search_for_keyword(keyword, brand.name):
+                results[keyword][brand.id] = {
+                    'id': brand.id,
+                    'type': 'Brand',
+                    'name': brand.name
+                }
+
     search_results['and_results'] = []
-    search_results['or_results'] = []
+    search_results['or_results'] = results
 
     return jsonify(search_results)
