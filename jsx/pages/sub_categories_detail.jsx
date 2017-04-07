e526cc4f24af39c047545e6fbd2854b7e4ed1b4c
Drew Romanyk <drewiswaycool@gmail.com>
2017-04-07 16:24:16 -0500

    Add sub categories front-end

---
+++ jsx/pages/sub_categories_detail.jsx
@@ -0,0 +1,91 @@
+/*global $*/ //tells ESLint that $ is a global object and is fine to use undefined
+import React, {PropTypes} from 'react';
+
+export default class Sub_Categories_Details extends React.Component {
+    constructor(props) {
+        super(props);
+        this.id = props.id;
+        this.state = {
+            sub_category: {
+                id: 0,
+                products: [],
+                brands: [],
+                tags: [],
+                name: '',
+                num_products: 0,
+                category: {
+                    id: 0,
+                    name: ''
+                },
+                avg_price: '',
+                avg_rating: ''
+            }
+        };
+    }
+
+    componentDidMount() {
+        $.getJSON(document.location.origin + '/api/sub_categories/' + this.id)
+            .then((data) => {
+                this.setState({sub_category: data});
+            });
+    }
+
+    render() {
+        const brands = this.state.sub_category.brands.map(item => {
+            return (
+                <span key={item.id} className='label label-primary'>
+                    <a href={'/tags/' + item.id}>{item.name}</a>
+                </span>
+            );
+        });
+        const products = this.state.sub_category.products.map(item => {
+            return (
+                <span key={item.id} className='label label-primary'>
+                    <a href={'/products/' + item.id}>{item.name}</a>
+                </span>
+            );
+        });
+        const tags = this.state.sub_category.tags.map(item => {
+            return (
+                <span key={item.id} className='label label-primary'>
+                    <a href={'/tags/' + item.id}>{item.name}</a>
+                </span>
+            );
+        });
+        return (
+            <div className="container">
+
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h1 className="panel-title">{this.state.sub_category.name}</h1>
+                    </div>
+                    <div className="panel-body">
+                        <h5>Average price: </h5>
+                        <p>{Number(this.state.sub_category.avg_price).toFixed(2)}</p>
+                        <h5>Average rating: </h5>
+                        <p>{Number(this.state.sub_category.avg_rating).toFixed(2)}</p>
+                        <h5>Category: </h5>
+                        <span key={this.state.sub_category.category.id} className='label label-primary'>
+                            <a href={'/categories/' + this.state.sub_category.category.id}>{this.state.sub_category.category.name}</a>
+                        </span>
+                        <h5>Brands: </h5>
+                        <div className="thumbnail horizontal-container">
+                            {brands}
+                        </div>
+                        <h5>Products: </h5>
+                        <div className="thumbnail horizontal-container">
+                            {products}
+                        </div>
+                        <h5>Tags: </h5>
+                        <div className="thumbnail horizontal-container">
+                            {tags}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+Sub_Categories_Details.propTypes = {
+    id: PropTypes.string.isRequired,
+};
