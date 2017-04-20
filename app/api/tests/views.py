a3c09b647cfacc45276e464690e4b535efda5e1c
Drew Romanyk <drewiswaycool@gmail.com>
2017-04-20 12:43:49 -0500

    Add coverage to run unit test on about page

--- app/api/tests/views.py
+++ app/api/tests/views.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 import unittest
-import io
 from app.tests import suite
+import coverage
 
 api_test_blueprints = Blueprint(
     'api_test', __name__
@@ -13,10 +13,15 @@ test_data = {
 
 
 @api_test_blueprints.route('/api/test')
-def get_tests(): # pragma: no cover
+def get_tests():  # pragma: no cover
     # Run tests.py here
+
     with open("unit-test.cache", "w") as stream:
+        c = coverage.Coverage()
+        c.start()
         unittest.TextTestRunner(stream=stream, verbosity=2).run(suite())
+        c.stop()
+        c.report(file=stream)
     with open("unit-test.cache") as stream:
         test_data['result'] = stream.read()
     return jsonify(test_data)
